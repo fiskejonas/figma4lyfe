cdd59514f8bfcc9d69233388a4952ef8827aa60e
”Jakob <jako500n@stud.kea.dk>
2022-11-24 09:42:20 +0100

    farver indsat

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        firmaroed: "#C0363F",
+        gul: "#D1B245",
+        groen: "#449B71",
+        moerkegraa: "#322C2C",
+        hvid: "#F0F0F0",
+      },
+    },
   },
   plugins: [],
 };
