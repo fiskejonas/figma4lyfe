a72c9c0a72c6174ab66f06a3c3ee9ec8948e7de8
”Jakob <jako500n@stud.kea.dk>
2022-11-24 11:25:58 +0100

    pusher alt

---
+++ .temp.1669281057897.tailwind.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        firmaroed: "#C0363F",
+        gul: "#D1B245",
+        groen: "#449B71",
+        moerkegraa: "#322C2C",
+        hvid: "#F0F0F0",
+      },
+    },
+    fontSize: {
+      3xl: ["67px", { lineHeight: "66px", letterSpacing: "-0.063em" }],
+    },
+    fontFamily: {
+      dosis: "Dosis, sans-serif",
+      vasirman: "Vazirmatn, sans-serif",
+    },
+  },
+  plugins: [],
+};
